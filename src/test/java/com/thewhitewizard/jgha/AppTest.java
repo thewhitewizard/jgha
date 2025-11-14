39197805c19de2e21d10dd36b502767ecbbd3e8d
Frederic CORDIER <frederic.cordier@iex.ec>
2025-11-14 14:04:56 +0100

    feat: init

---
+++ src/test/java/com/thewhitewizard/jgha/AppTest.java
@@ -0,0 +1,14 @@
+
+package com.thewhitewizard.gha;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+
+    @Test
+    void justASimpleTest() {
+        assertTrue(true);
+    }
+}
